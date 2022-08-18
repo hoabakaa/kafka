f4c40a0ee0e3638111435de0c698e649e7531772
longcn <son.trieu@adamosoft.com>
2022-08-18 17:13:59 +0700

    consumer config

--- src/main/java/com/example/kafkademo/KafkaDemoApplication.java
+++ src/main/java/com/example/kafkademo/KafkaDemoApplication.java
@@ -16,7 +16,7 @@ public class KafkaDemoApplication {
     @Bean
     CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
         return args -> {
-            for (int i = 0; i < 10000000; i++) {
+            for (int i = 0; i < 100; i++) {
                 kafkaTemplate.send("longCode", "Hello Kafka :)" +i);
 
             }
